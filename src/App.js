9ed1ea403143231daf4f0d7b8f7487d7116910b3
Claudio Alexis <irianniclaudio@gmail.com>
2022-07-25 19:48:58 -0300

    delete 'robots' and use test API

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
+      .then((users) => this.setState({ robots:users }));
   }
 
   onSearchChange = (e) => {
@@ -28,13 +28,17 @@ class App extends Component {
         .toLocaleLowerCase()
         .includes(this.state.searchfield.toLocaleLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
